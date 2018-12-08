4399f95c6ec89030c30701d99a8fe9bbf4f6d73e
Matthew Segal <mattdsegal@gmail.com>
2018-12-08 15:49:34 +1100

    clean up scripts

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -2,5 +2,3 @@
 . "$(dirname $0)/settings.sh"
 echo "Stopping EC2"
 aws ec2 stop-instances --instance-ids $INSTANCE_ID
-echo "EC2 Status"
-aws ec2 describe-instance-status --instance-ids $INSTANCE_ID
